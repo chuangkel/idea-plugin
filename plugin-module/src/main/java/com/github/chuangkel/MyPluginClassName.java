acb9d851ea5bb51d2a60ae94d28423654dae0f80
2010344124@qq.com <2010344124@qq.com>
2020-06-04 15:52:47 +0800

    init

---
+++ plugin-module/src/main/java/com/github/chuangkel/MyPluginClassName.java
@@ -0,0 +1,21 @@
+package com.github.chuangkel;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+public class MyPluginClassName extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        Project project = e.getData(PlatformDataKeys.PROJECT);
+        Messages.showInputDialog(
+                project,
+                "What is your name?",
+                "Input your name",
+                Messages.getQuestionIcon());
+    }
+}
